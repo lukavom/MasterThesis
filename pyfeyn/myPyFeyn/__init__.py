e61bf8dd5c156f7643544c7230b9a544d8d39d7d
Ian Brock <ian.brock@cern.ch>
2020-05-29 16:35:42 +0200

    Small tweaks to Feynman graphs. Started reading through guide. Eliminate more old TeX Live stuff.

--- pyfeyn/myPyFeyn/__init__.py
+++ pyfeyn/myPyFeyn/__init__.py
@@ -6,6 +6,8 @@ from .draw_stuff import draw_fermion, draw_boson, draw_gluon, draw_higgs
 __all__ = ['draw_fermion', 'draw_boson', 'draw_gluon', 'draw_higgs']
 
 # Define font and colours for LaTeX
+# defaulttexrunner has to be replaced by defaulttextengine with PyX 0.15
+# Similar fixes are also necessary in the PyFeyn package
 # pyx.text.defaulttexrunner.preamble(r"\usepackage[varg]{txfonts}")
 pyx.text.defaulttexrunner.preamble(r"\usepackage{newtxtext}")
 pyx.text.defaulttexrunner.preamble(r"\usepackage{newtxmath}")
@@ -16,7 +18,8 @@ pyx.text.defaulttexrunner.preamble(r"\usepackage{newtxmath}")
 # pyx.text.defaulttexrunner.preamble(r"\renewcommand{\rmdefault}{\sfdefault}")
 pyx.text.defaulttexrunner.preamble(r"\usepackage[T1]{fontenc}")
 pyx.text.defaulttexrunner.preamble(r"\usepackage[dvipsnames]{xcolor}")
-pyx.text.defaulttexrunner.preamble(r"\usepackage[italic]{hepnicenames}")
+# Use this line if hepnicenames removed from pyfeyn/__init__.py
+# pyx.text.defaulttexrunner.preamble(r"\usepackage[italic]{heppennames}")
 # Adjust the kerning for bar over particles with narrow glyphs (from Andy Buckley)
 # This is probably only needed with the italic option
 pyx.text.defaulttexrunner.preamble(r"\makeatletter\def\@shiftlen@anti@gen@bar{0mu}\makeatother")
